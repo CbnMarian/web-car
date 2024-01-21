381c766a3f40baefbb129d81f80c6d3f51575326
CbnMarian <m.marian03@yahoo.com>
2024-01-21 20:28:40 +0200

    5

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Contact" element={<Contact />} />
