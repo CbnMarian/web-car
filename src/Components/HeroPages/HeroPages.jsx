fc6451b88f2f101ea80c8e338e1bba90f5a1154d
CbnMarian <m.marian03@yahoo.com>
2024-01-24 11:55:45 +0200

    3

--- src/Components/HeroPages/HeroPages.jsx
+++ src/Components/HeroPages/HeroPages.jsx
@@ -1,10 +1,16 @@
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./HeroPages.css";
 
 function HeroPages({ name }) {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
     <>
-      <section className="hero-pages">
+      <section className="hero-pages" data-aos="fade-down">
         <div className="hero-pages__overlay"></div>
         <div className="container">
           <div className="hero-pages__text">
